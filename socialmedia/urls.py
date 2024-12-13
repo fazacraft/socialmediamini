ea1da1754d3a8202daf0361a7068544d47d20916
Fazliddin <phazliddinphozilov@gmail.com>
2024-12-13 14:12:59 +0500

    path for comment added to urls

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import home_view, profile_view, setting_view, signin_view, signup_view, signout_view, upload_image_view, \
-    post_view
+    post_view, comment_view
 
 urlpatterns = [
     path('', home_view),
@@ -10,7 +10,8 @@ urlpatterns = [
     path('signup/',signup_view),
     path('signout/', signout_view),
     path('upload_image/', upload_image_view),
-    path('post/', post_view)
+    path('post/', post_view),
+    path('comment/<int:pk>/', comment_view)
 ]
 
 
