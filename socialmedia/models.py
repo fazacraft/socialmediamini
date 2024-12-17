bab9ff7f9ae741bdbee16934f901cf3acf845480
Fazliddin <phazliddinphozilov@gmail.com>
2024-12-18 02:32:49 +0500

    fixed any issues in models with Post model

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -10,14 +10,20 @@ class Post(models.Model):
     text = models.TextField(blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now =True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
-
-
+    def likes_count(self):
+        return self.likes.count() -1
 
     def __str__(self):
         return self.author.username
+
+    def comments_count(self):
+        return self.comments.count()
+
+    def like_username(self):
+        return self.author_like.username
 
 
 class Comment(models.Model):
@@ -35,15 +41,14 @@ class Comment(models.Model):
 
 
 class UserLikePost(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='author_like')
-    post = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_like')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
-        return self.author.username
+        return self.post.author.username
 
 
 class UserFollow(models.Model):
@@ -53,15 +58,13 @@ class UserFollow(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.follower.username
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    picture = models.ImageField(upload_to='profiles/',default='nonegigachad.webp' )
+    picture = models.ImageField(upload_to='profiles/', default='nonegigachad.webp')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
