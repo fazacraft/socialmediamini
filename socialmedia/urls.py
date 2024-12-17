2455caed98612710b1406270d21ccb09cbba7a31
Fazliddin <phazliddinphozilov@gmail.com>
2024-12-18 02:35:30 +0500

    addded like , delete_post, follow paths

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import home_view, profile_view, setting_view, signin_view, signup_view, signout_view, upload_image_view, \
-    post_view, comment_view
+    post_view, comment_view, like_view, delete_post_view, follow_view
 
 urlpatterns = [
     path('', home_view),
@@ -11,7 +11,11 @@ urlpatterns = [
     path('signout/', signout_view),
     path('upload_image/', upload_image_view),
     path('post/', post_view),
-    path('comment/<int:pk>/', comment_view)
+    path('comment/<int:pk>/', comment_view),
+    path('like/',like_view),
+    path('delete_post/', delete_post_view),
+    path('follow/', follow_view),
+
 ]
 
 
